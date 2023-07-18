253403884179c3eba02438f3c5ad1474f656c6b8
PraveenKumar-10 <132678587+PraveenKumar-10@users.noreply.github.com>
2023-07-18 21:04:49 +0530

    Add files via upload

---
+++ assignmentweek1day2/Mobile.java
@@ -0,0 +1,23 @@
+package assignmentweek1day2;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		String mobileBrandName = "POCO";
+		System.out.println("Brand Name             :" + mobileBrandName);
+		char mobileLogo = 'P';
+		System.out.println("Mobile LOGO            :" + mobileLogo);
+		short noOfMobilePiece = 5;
+		System.out.println("Number of Mobile Piece :" + noOfMobilePiece);
+		int modelNumber = 3064;
+		System.out.println("Model Number           :" + modelNumber);
+		long mobileImeiNumber = 841941387;
+		System.out.println("IMEI Number            :" + mobileImeiNumber);
+		float mobilePrice = 19000.0F;
+		System.out.println("Price                  :" + mobilePrice);
+		boolean isDamaged = true;
+		System.out.println("Damage                 :" + isDamaged);
+
+	}
+
+}
---
+++ assignmentweek1day2/TwoWheeler.java
@@ -0,0 +1,21 @@
+package assignmentweek1day2;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+		int noOfWheels = 2;
+		System.out.println(noOfWheels);
+		short noOfMirrons = 2;
+		System.out.println(noOfMirrons);
+		long chassisNumber = 824587656L;
+		System.out.println(chassisNumber);
+		boolean isPunctured = false;
+		System.out.println(isPunctured);
+		String bikeName = "BMW";
+		System.out.println(bikeName);
+		double runningKM = 1300.0;
+		System.out.println(runningKM);
+
+	}
+
+}
